23d1d5a48232ce9d38b65b5981207614fdb0d4af
George Heavenson Jnr. Oti-Adjei <jrgeorge991@outlook.com>
2023-10-10 01:03:58 +0000

    Update

--- delete-func.js
+++ delete-func.js
@@ -1,39 +1,50 @@
-$(document).ready(() => {
-  const alertBox = $("#alert-box");
-  const productForm = $("#product_form");
-  const productCheckbox = $(".delete-checkbox");
+// Description: This file contains the code for the delete function
+// get all the checkboxes and create an empty array
 
-  if (productCheckbox.length == 0) {
-    return;
-  }
+let checkboxes = document.querySelectorAll(".delete-checkbox");
+let checkedList = [];
 
-  let productSKUs = [];
-
-  for (let i = 0; i < productCheckbox.length; i++) {
-    if (productCheckbox[i].checked) {
-      productSKUs.push(productCheckbox[i].value);
-      productCheckbox[i].parentElement.parentElement.remove();
+//loop through the checkboxes 
+for (let checkbox of checkboxes) {
+  //add event listener to each checkbox to know when it is checked or unchecked
+  checkbox.addEventListener("click", function () {
+    //add the checked ones to the array
+    if (this.checked == true) {
+      console.log(this.value);
+      checkedList.push(this.value);
+      console.log(checkedList);
     }
-    console.log(productSKUs);
-  }
-  let list = {
-    skus: productSKUs,
-  };
-
-  // delete product
-  $("#delete-product-btn").on("click", () => {
-    $.ajax({
-      method: "POST",
-      url: "/api/delete.php",
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(list),
-      dataType: "json",
-      success: function (res) {
-        window.location.href = "/";
-      },
-      error: function (error) {
-        console.log(error);
-      },
-    });
+    //remove the unchecked ones from the array
+    else {
+      var index = checkedList.indexOf(this.value);
+      if (index > -1) {
+        checkedList.splice(index, 1);
+        console.log(checkedList);
+      }
+    }
   });
-});
+}
+
+let data = {
+  "skus": checkedList
+};
+
+//function to delete the checked products by sending the array to the api
+// delete product
+    $('#delete-product-btn').on('click', () => {
+        
+
+        $.ajax({
+            method: 'POST',
+            url: '/api/delete.php',
+            contentType: "application/json; charset=utf-8",
+            data: JSON.stringify(data),
+            dataType: 'json',
+            success: function (res) {
+                window.location.href = "/"
+            },
+            error: function (error) {
+                 console.log(error);
+             }
+        });
+    });
