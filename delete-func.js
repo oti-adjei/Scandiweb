eecac4a5f3b49603a87337cc5256b5b33e18ee8d
George Heavenson Jnr. Oti-Adjei <jrgeorge991@outlook.com>
2023-10-11 01:16:41 +0000

    Changed delete func to aync await fetch

--- delete-func.js
+++ delete-func.js
@@ -2,54 +2,37 @@
 // get all the checkboxes and create an empty array
 
 let checkboxes = document.querySelectorAll(".delete-checkbox");
-let checkedList = [];
+// let checkedList = [];
 
 //loop through the checkboxes 
-for (let checkbox of checkboxes) {
-  //add event listener to each checkbox to know when it is checked or unchecked
-  checkbox.addEventListener("click", function () {
-    //add the checked ones to the array
-    if (this.checked == true) {
-      console.log(this.value);
-      checkedList.push(this.value);
-      console.log(checkedList);
+$('#delete-product-btn').on('click', async () => {
+    let data = {
+        skus: []
+    };
+    let checkboxes = document.querySelectorAll(".delete-checkbox");
+    for (let checkbox of checkboxes) {
+        if (checkbox.checked) {
+            data.skus.push(checkbox.value)
+        }
     }
-    //remove the unchecked ones from the array
-    else {
-      var index = checkedList.indexOf(this.value);
-      if (index > -1) {
-        checkedList.splice(index, 1);
-        console.log(checkedList);
-      }
+    try{
+        const res = await fetch('api/delete.php', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
+        const resObj = await res.json();
+        if (resObj.message === "Product deleted successfully") {
+            alert("Product deleted successfully");
+        } 
+        else {
+            alert("Failed to delete product");
+        }
     }
-  });
-}
-
-let data = {
-  "skus": checkedList
-};
-
-//function to delete the checked products by sending the array to the api
-// delete product
-$('#delete-product-btn').on('click', () => {
-  $.ajax({
-      method: 'POST',
-      url: 'api/delete.php',
-      contentType: "application/json; charset=utf-8",
-      data: JSON.stringify(data),
-      dataType: 'json',
-      success: function (res) {
-          if (res.message === "Product deleted successfully") {
-              alert("Product deleted successfully");
-              
-          } else {
-              alert("Failed to delete product");
-          }
-      },
-      error: function (error) {
-          console.log("Error:", error);
-          alert("An error occurred while deleting the product.");
-      }
-  });
+    catch(error){
+        console.log("Error:", error);
+        alert("An error occurred while deleting the product.");
+    }
 });
-    
